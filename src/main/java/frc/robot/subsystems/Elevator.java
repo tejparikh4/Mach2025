1699544c1d3dc3c0ae74fac178e59a271dbe4e00
AU199 <you@example.com>
2025-02-13 18:59:19 -0500

    Elevator code

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -5,6 +5,9 @@ import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -14,7 +17,18 @@ public class Elevator extends SubsystemBase{
     private SparkMax rightMotor;
     private SparkClosedLoopController leftController;
     private SparkClosedLoopController rightController;
-
+    private static double kDt = 0.02;
+    private static double kMaxVelocity = 1.75;
+    private static double kMaxAcceleration = 0.75;
+    private static double kP = 1.3;
+    private static double kI = 0.0;
+    private static double kD = 0.7;
+    private static double kS = 1.1;
+    private static double kG = 1.2;
+    private static double kV = 1.3;
+    private final TrapezoidProfile.Constraints m_Constraints = new TrapezoidProfile.Constraints(1.5, 0.75);
+    private final ProfiledPIDController m_Controller = new ProfiledPIDController(kP, kI, kD, m_Constraints);
+    private final ElevatorFeedforward m_Feedforward =  new ElevatorFeedforward(kS, kG, kV);
     public Elevator() {
         leftMotor = new SparkMax(Constants.elevatorLeftId, MotorType.kBrushless);
         rightMotor = new SparkMax(Constants.elevatorRightId, MotorType.kBrushless);
@@ -24,11 +38,10 @@ public class Elevator extends SubsystemBase{
     }
 
     public Command moveToHeight(double height){
-        double rotations = 1; //height / (Constants.kElevatorGearDiameter * Math.PI) * Constants.kElevatorGearRatio;
-        return run(() -> {
-            leftController.setReference(rotations, SparkBase.ControlType.kPosition);
-            rightController.setReference(rotations, SparkBase.ControlType.kPosition);
-        });
+        m_Controller.setGoal(height);
+        return run(() -> leftMotor.setVoltage(m_Controller.calculate(leftMotor.getAbsoluteEncoder().getPosition())+m_Feedforward.calculate(m_Controller.getSetpoint().velocity)));
+
     }
+    
     
 }
