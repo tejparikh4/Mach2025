5a1956a75af7de84c993cd284ca13c6d4808c397
AU199 <you@example.com>
2025-03-18 19:43:26 -0400

    pathing like baller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -80,9 +80,9 @@ public class RobotContainer {
     NamedCommands.registerCommand("L2",
       arm.moveToPosition(Constants.transitionRotation).andThen(
       elevator.moveToHeight(Constants.L2Height)).andThen(
-      arm.moveToPosition(Constants.L2Rotation)).andThen(
-        arm.outtake(.25))
+      arm.moveToPosition(Constants.L2Rotation))
     );
+    NamedCommands.registerCommand("Outtake", arm.outtake(0.25));
 
     chooserAuto = new SendableChooser<String>();
     chooserAuto.setDefaultOption("nothing", "nothing");
