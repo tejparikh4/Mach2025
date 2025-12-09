5ddf679b606c12ae4043341ee1557dc255c51939
AU199 <akulgmvl@gmail.com>
2025-12-09 18:29:11 -0500

    Added some code that is from the examples, gets the best estimated position and prints the x and y position

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
-
-import java.util.function.DoubleSupplier;
-
-import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
@@ -14,8 +12,7 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class Arm extends SubsystemBase {
    private SparkMax pivotMotor;
--- src/main/java/frc/robot/subsystems/photon.java
+++ src/main/java/frc/robot/subsystems/photon.java
@@ -13,9 +13,11 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -35,29 +37,39 @@ public class photon  extends SubsystemBase{
         this.swerveDriveBase = swerveDriveBase;
         this.camera = new PhotonCamera("photonvision");
         this.estConsumer = estConsumer;
-          photonEstimator =
+        photonEstimator =
                 new PhotonPoseEstimator(Constants.kTagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, Constants.kRobotToCam);
         photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
     }
     public void getResults(){
-            Optional<EstimatedRobotPose> visionEstimate = Optional.empty();
-        for (PhotonPipelineResult change:camera.getAllUnreadResults()){
-            visionEstimate = photonEstimator.update(change);
-            if (RobotBase.isSimulation()){
-                visionEstimate.ifPresentOrElse(est ->
-                getSimDebugField()
-                        .getObject("VisionEstimation")
-                        .setPose(est.estimatedPose.toPose2d()));
-            }
+        var visionEstimates = camera.getAllUnreadResults();
+        for (var visionEstimate: visionEstimates){
+            var multiTagVision = visionEstimate.multitagResult.get();
+            var transform3dPos = multiTagVision.estimatedPose.best;
+            Pose2d pose2dPos = transform3dToPose2d(transform3dPos);
+            System.out.println("Estimated x: " +pose2dPos.getX() + "\nEstimated y: "+pose2dPos.getY() + "\nEstimated yaw: "+ pose2dPos.getRotation());
+            // swerveDrive.addVisionMeasurement(pose2dPos, 0);
         }
-    }
 
+    }
+    public Pose2d transform3dToPose2d(Transform3d transforming){
+        return new Pose2d(transforming.getMeasureX(),transforming.getMeasureY(),transforming.getRotation().toRotation2d());
+    }
     
     
+    public Field2d getSimDebugField() {
+        if (!Robot.isSimulation()) return null;
+        return null;
+    }
+        
     
     @FunctionalInterface
     public static interface EstimateConsumer {
         public void accept(Pose2d pose, double timestamp, Matrix<N3, N1> estimationStdDevs);
+    }
+    @Override
+    public void periodic(){
+        getResults();
     }
 
 }
